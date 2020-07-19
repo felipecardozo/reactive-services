8cf73796196f81c653bf52427086e29c763f6aef
Felipe Cardozo <felipe.cardozo@gmail.com>
2020-07-19 14:02:08 -0500

    initial structure

---
+++ src/main/java/com/reactive/beta/version/DemoForReactiveServiccesBetaVersionApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.beta.version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForReactiveServiccesBetaVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForReactiveServiccesBetaVersionApplication.class, args);
+	}
+
+}
